96cfb32223a49628be477b7ae3c660de10e2497f
Rayengharby <rayen14865@gmail.com>
2024-03-20 17:54:40 +0100

    PosController with ressource and Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('posts', PostController::class);
+
+
