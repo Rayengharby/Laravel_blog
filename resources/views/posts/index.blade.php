b3cd9b78e4f0c699e8e3fc167a3b3c1d2d39ce1e
Rayengharby <rayen14865@gmail.com>
2024-03-21 23:22:33 +0100

    Réorganiser les templates

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-  @include('layouts.navigation');
-    <div class="container py-4">
+
+  @extends('layouts.app')
+  @section('content')
 <table class="table">
   <thead>
     <tr>
@@ -34,8 +26,5 @@
     </tr>
     @endforeach
   </tbody>
-  </div>
 </table>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-<body>
-    @include('layouts.navigation');
+@extends('layouts.app')
+
+@section('content')
     <div class="container py-4" style="width: 18rem;">
         <img src="{{ $post->image }}" class="card-img-top" alt="{{ $post->title }}">
         <div class="card-body">
@@ -25,7 +17,5 @@
             </p>
           {{-- <a href="#" class="btn btn-primary">{{ $post->content }}</a> --}}
         </div>
-      </div>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</html>
+    </div>
+ @endsection
